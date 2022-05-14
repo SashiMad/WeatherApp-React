0f6d5f675da73f867a298f0c74c971a5ddf57dfb
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-05-14 17:09:51 +0200

    Integrated API for the weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       date: new Date(response.data.dt * 1000),
       city: response.data.name,
       temperature: response.data.main.temp,
@@ -69,7 +70,7 @@ export default function Weather(props) {
             </div>
           </form>
           <WeatherInfo data={weatherData} />
-          <WeatherForecast />
+          <WeatherForecast coordinates={weatherData.coordinates} />
         </div>
         <p className="footer">
           <a
